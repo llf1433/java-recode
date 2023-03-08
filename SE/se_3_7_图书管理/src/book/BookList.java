ebac20312fdea8285aafa692c1bfbcbac65b906a
llf114514 <yxd020202@163.com>
2023-03-08 15:51:34 +0800

    完善

--- SE/se_3_7_图书管理/src/book/BookList.java
+++ SE/se_3_7_图书管理/src/book/BookList.java
@@ -2,7 +2,7 @@ package book;
 
 public class BookList {
     public Book[] books = new Book[10];//书的数组,设定大小
-    public int usedSize; //存储书的数量
+    private int usedSize; //存储书的数量
 
     /**
      * 通过构造，初始化时预先在数组存储三本书，usedSize = 3
@@ -12,6 +12,17 @@ public class BookList {
         books[1] = new Book("西游记", "吴承恩", 49, "小说" );
         books[2] = new Book("红楼梦" , "曹雪芹", 49, "小说");
         this.usedSize = 3;
+    }
+    public int getUsedSize() {
+        return this.usedSize;
+    }
+
+    public void setUsedSize(int usedSize) {
+        this.usedSize = usedSize;
+    }
+
+    public Book getPos(int pos) {
+        return books[pos];
     }
 
 
--- SE/se_3_7_图书管理/src/operations/FindOperation.java
+++ SE/se_3_7_图书管理/src/operations/FindOperation.java
@@ -1,8 +1,24 @@
 package operations;
+import book.Book;
 import book.BookList;
+import java.util.Scanner;
+
 public class FindOperation implements IOperation{
     public void work(BookList bookList) {
         //查找图书
-        System.out.println("查找图书");
+        //System.out.println("查找图书");
+        System.out.println("输入你要查找的图书名：");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        for (int i = 0; i < bookList.getUsedSize(); i++) {
+            Book books = bookList.getPos(i);
+            if (name.equals(books.getName())) {
+                System.out.println("找到了这本书！");
+                System.out.println(books.toString());
+                return;
+            }
+        }
+        System.out.println("没找到！");
+        return;
     }
 }
