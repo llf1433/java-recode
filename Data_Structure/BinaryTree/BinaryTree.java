35b267e197f2ae5a9c22f51259ee29303e68ebe9
llf114514 <yxd020202@163.com>
2023-03-22 22:10:28 +0800

    二叉树

--- Data_Structure/BinaryTree/BinaryTree.java
+++ Data_Structure/BinaryTree/BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
     static class TreeNode {
         public int value;
@@ -7,7 +10,6 @@ public class BinaryTree {
             this.value = value;
         }
     }
-
 
     TreeNode createTree() {
         TreeNode t1 = new TreeNode(1);
@@ -22,6 +24,7 @@ public class BinaryTree {
 
     TreeNode root = createTree();
 
+    //前序遍历
     void prevOrder(TreeNode root) {
         if (root == null) {
             return;
@@ -31,6 +34,8 @@ public class BinaryTree {
         prevOrder(root.right);
     }
 
+
+    //中序遍历
     void inOrder(TreeNode root) {
         if (root == null) {
             return;
@@ -40,6 +45,7 @@ public class BinaryTree {
         inOrder(root.right);
     }
 
+    //后序遍历
     void postOrder(TreeNode root) {
         if (root == null) {
             return;
@@ -49,8 +55,17 @@ public class BinaryTree {
         System.out.print(root.value + " ");
     }
 
+    //获取结点个数
+    int getNodeCount(TreeNode root) {
+        /*
+          子问题--(左结点个数 + 右结点个数)
+         */
+        if (root == null) {
+            return 0;
+        }
+        return getNodeCount(root.left) + getNodeCount(root.right) + 1;
 
-
+    }
 
     //获取K层结点个数
     int getKlevelNodeCount(TreeNode root, int k) {
@@ -63,14 +78,67 @@ public class BinaryTree {
         return getKlevelNodeCount(root.left, k--) + getKlevelNodeCount(root.left, k--);
     }
 
+    //获取高度
+    int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //重复递归
+        /*return getHeight(root.left) > getHeight(root.right) ?
+                getHeight(root.left) + 1 : getHeight(root.right) + 1;*/
+
+        int lengthHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        return lengthHeight > rightHeight ? lengthHeight + 1 : rightHeight + 1;
+    }
+
+
+
+    //检测value是否存在
+    public boolean isExist(TreeNode root, int value) {
+        if (root == null) {
+            return false;
+        }
+        if (root.value == value) {
+            return true;
+        }
+        return isExist(root.left, value) || isExist(root.right, value);
+    }
+
+    //层序遍历
+    public void  levelOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            System.out.print(cur.value + " ");
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+        System.out.println();
+    }
+
+
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
         BinaryTree.TreeNode root = binaryTree.createTree();
         System.out.println(binaryTree.getKlevelNodeCount(root,3));
         binaryTree.prevOrder(root);
         System.out.println();
-        binaryTree.inOrder(root);
+        /*binaryTree.inOrder(root);
         System.out.println();
         binaryTree.postOrder(root);
+        System.out.println();
+        System.out.println(“结点总数：” + binaryTree.getNodeCount(root));*/
+        System.out.println("树的高度" + binaryTree.getHeight(root));
+        binaryTree.levelOrder(root);
+        System.out.println(binaryTree.isExist(root,1));
     }
 }
