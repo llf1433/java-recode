2d7fbd2ec314302f41182bbe08e40ab2fa4f2178
llf114514 <yxd020202@163.com>
2023-03-27 21:44:26 +0800

    选择排序

---
+++ Data_Structure/Sort/SelectSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+/**
+ *  选择排序
+ *  时间复杂度：O(n^2)
+ *  空间复杂度：O(1)
+ *  稳定性：稳定
+ */
+public class SelectSort {
+    public static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int mi = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[mi]) {
+                    mi = j;
+                }
+            }
+            if (i != mi) {
+                swap(array,i, mi);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = new int[]{9,8,7,6,1,0};
+        SelectSort.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
