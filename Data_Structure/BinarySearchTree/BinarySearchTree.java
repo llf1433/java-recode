2ee60057a4a09023385d25678443b7893300bb27
llf114514 <yxd020202@163.com>
2023-03-31 20:47:43 +0800

    二叉搜索树

---
+++ Data_Structure/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+
+public class BinarySearchTree {
+
+    static class TreeNode {
+        public int key;//
+        public TreeNode left;
+        public TreeNode right;
+
+        TreeNode(int key) {
+            this.key = key;
+        }
+    }
+
+    public TreeNode root;
+
+    /**
+     * 插入一个元素
+     * @param key
+     */
+    public boolean insert(int key) {
+
+        return true;
+    }
+    //查找key是否存在
+    public TreeNode search(int key) {
+
+        return null;
+    }
+    //删除key的值
+    public boolean remove(int key) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
