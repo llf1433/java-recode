a7982463808264318f76697c6c6615cd04470dcb
llf114514 <yxd020202@163.com>
2023-04-14 23:20:11 +0800

    update

--- MySQL/JDBC/JDBCInsertDemo.java
+++ MySQL/JDBC/JDBCInsertDemo.java
@@ -1,28 +1,60 @@
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import javax.sql.DataSource;
-import java.sql.Connection;//--连接
+import java.sql.Connection; //--连接
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class JDBCInsertDemo {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException{
         /*
             数据库(llf) 数据表()
          */
-        //创建数据源 -- 数据库的位置
+        //1.创建数据源 -- 数据库的位置
         DataSource dataSource = new MysqlDataSource();
-        // 设置数据库的位置
-        ((MysqlDataSource)dataSource).setURL("jdbc:mysql://127.0.0.1:3306/llf?characterEncoding = utf8 & useSSL = false");
+        /*
+            设置数据库的位置
+            用户名和密码
+         */
+        ((MysqlDataSource)dataSource).setURL("jdbc:mysql://127.0.0.1:3306/llf?characterEncoding=utf8&useSSL=false");
         ((MysqlDataSource)dataSource).setUser("root");
         ((MysqlDataSource)dataSource).setPassword("Llf.172839");
 
-        //连接数据库
-        try {
-            Connection connection = dataSource.getConnection();
-        } catch(SQLException ignored) {
 
-        }
+        //2.连接数据库
+        Connection connection = dataSource.getConnection();
 
+        //3.构造SQL语句
+        //String sql = "insert into student values ('张三', 18),('李四',19)";
+        String sql = "insert into student values (?,?)";
+
+
+        /*
+            用一个特定对象描述sql语句
+        */
+        PreparedStatement statement = connection.prepareStatement(sql);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("输入姓名:");
+        String name = scanner.next();
+        int age = scanner.nextInt();
+
+
+        statement.setString(1,name);
+        /*
+            4.执行sql
+            执行增,删,改使用executeUpdate()
+                    查使用executeQuery()
+            ret--影响的行(row)数
+         */
+        int ret = statement.executeUpdate();
+        System.out.println(ret);
+        /*
+            5.断开连接,释放资源
+            手动释放
+            释放顺序与创建相反
+         */
+        statement.close();
+        connection.close();
     }
 
 }
